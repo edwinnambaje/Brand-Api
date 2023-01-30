5ff951babc7446e73d9493b6d55f20cf78c8dfc2
edwinnambaje <nambajeeedwin@gmail.com>
2023-01-30 20:40:53 +0200

    added helpers and also middleware

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const verify=require('../helpers/jwt')
+
+const verifyToken = (req,res,next) =>{
+    try {
+        const authHeader = req.headers.token;
+        const token = authHeader && authHeader.split(' ')[1];
+        if(!token) return res.status(401).json("You are not Authenticated!");
+        const verified = verify.verify(token);
+        req.user = verified;
+        next();
+    } catch (error) {
+        res.status(401).json({status:"error",error:error.message});
+    }
+}
+const verifyTokenAndRole = (req,res,next) =>{
+    verifyToken(req,res,() =>{
+        if(req.user.role === "admin"){
+            next();
+        }
+        else{
+            return res.status(401).json({status:"error",error:"You are not authenticated"});
+        }
+    })
+}
+module.exports={verifyToken,verifyTokenAndRole}
