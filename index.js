d8cb843a1d928d56e4f132387a5fc5a77e2dbb5a
edwinnambaje <nambajeeedwin@gmail.com>
2023-02-03 19:50:44 +0200

    api

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-const app=require('./app')
+const express=require('express');
+const mongoose=require('mongoose')
+const dotenv=require('dotenv');
+const authRoute=require('./routes/auth');
+const userRoute=require('./routes/user');
+const postRoute=require('./routes/post');
+const catRoute=require('./routes/category');
+const messageRoute=require('./routes/message');
+const swaggerDocs=require('./docs/swagger');
+const app=express();
+dotenv.config();
+app.use(express.json());
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_URL)
+.then(console.log('Connected to MongoDb'))
+.catch((error)=>console.log(error))
+swaggerDocs(app);
+app.use('/api/auth',authRoute);
+app.use('/api/user',userRoute);
+app.use('/api/posts',postRoute);
+app.use('/api/categories',catRoute)
+app.use('/api/messages',messageRoute);
 let port=process.env.PORT || 5000
 app.listen(port,()=>{
     console.log(`Backend is running on port${port}`);
